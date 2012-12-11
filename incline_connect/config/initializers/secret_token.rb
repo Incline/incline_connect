7c3f7231f88c6b86103f00893b4569b6d742b636
Corey Trombley <corey@quilible.com>
2012-12-11 13:18:26 -0500

    inital commit

---
+++ incline_connect/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InclineConnect::Application.config.secret_token = 'ce6aa351efab469e1a78444c49aea532156524c04e99550f0e41dc3c3bf74e74b4a90f2cd8218bb1d85ecfa05ddb17f41a13ac1badb7ad6216a1e96757e31b10'
