056eeb0830d3cc11126ed949ab6fba51b52b0f83
Stuart Wagner <stuwags@gmail.com>
2012-12-11 17:57:48 -0500

    annotated User model shit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,15 @@
 #
 # Table name: users
 #
-#  id          :integer          not null, primary key
-#  provider    :string(255)
-#  uid         :string(255)
-#  name        :string(255)
-#  profile_pic :string(255)
-#  location    :string(255)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id               :integer          not null, primary key
+#  provider         :string(255)
+#  uid              :string(255)
+#  name             :string(255)
+#  profile_pic      :string(255)
+#  location         :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  linkedin_profile :string(255)
 #
 
 class User < ActiveRecord::Base
